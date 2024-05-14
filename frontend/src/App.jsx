03c522b12eb57ad2aea4d0a63959a05ca32cd18e
Arsdev998 <arsdev998@gmail.com>
2024-05-14 15:05:12 +0700

    cler be

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+function App() {
+
+  return (
+    < >
+      <h1 className="text-red-500 mt-12 px-[200px] font-bold font-mono text-2xl">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem
+        praesentium aut deleniti ea provident nulla assumenda enim, possimus et
+        hic reiciendis <span className="text-yellow-500">repellat</span> at atque, fugit quis explicabo labore facere
+        aliquid.
+      </h1>
+    </>
+  );
+}
+
+export default App;
